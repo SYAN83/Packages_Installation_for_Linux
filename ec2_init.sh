fbe4a192a5864144c903b08155e8f2e979d4a782
SHU YAN <yanshu.usc@gmail.com>
2017-11-29 16:09:59 -0500

    add virtualenv

--- ec2_init.sh
+++ ec2_init.sh
@@ -33,3 +33,5 @@ sudo chmod +x /usr/local/bin/docker-compose
 curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
 chmod +x /tmp/docker-machine &&
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+# Install python3 virtualenv
+sudo apt-get -y install python3-venv
