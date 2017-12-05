f68d3afd4005c74d76b670d09578bf1c77c4ec36
SHU YAN <yanshu.usc@gmail.com>
2017-12-05 14:04:23 -0500

    Update and rename ec2_init.sh to aws_ec2_docker.sh

--- aws_ec2_docker.sh
+++ aws_ec2_docker.sh
@@ -3,10 +3,13 @@
 # Update the apt package index
 sudo apt-get update
 # Install git
+echo "Installing git..."
 sudo apt install git
 # Optional: git configuration
 # git config --global user.email "you@example.com"
 # git config --global user.name "Your Name"
+# Install docker
+echo "Installing docker..."
 # Install packages to allow apt to use a repository over HTTPS
 sudo apt-get -y install \
   apt-transport-https \
@@ -27,10 +30,13 @@ sudo apt-get -y install docker-ce
 # Add the current user to docker group
 sudo usermod -aG docker $USER
 # Install Compose
+echo "Installing docker compose..."
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # Install Docker Machine
+echo "Installing docker machine..."
 curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
 chmod +x /tmp/docker-machine &&
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
 
